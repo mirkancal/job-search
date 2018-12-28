964132bd6de69eee81c1835ca4697d38ff5cede4
onat_cipli <onatcipli@outlook.com>
2018-12-28 09:32:41 +0300

    firebase added

--- App.js
+++ App.js
@@ -1,3 +1,98 @@
+let firebaseUserID;
+
+firebase.auth().onAuthStateChanged(function (user) {
+    if (user) {
+        // User is signed in.
+        firebaseUserID = user.uid;
+
+        document.getElementById("user_div").style.display = "block";
+        document.getElementById("search_div").style.display = "block";
+        document.getElementById("login_div").style.display = "none";
+        document.getElementById("sign_up_div").style.display = "none";
+
+        var user = firebase.auth().currentUser;
+
+        if (user != null) {
+
+            var email_id = user.email;
+            document.getElementById("user_para").innerHTML = "Welcome User : " + email_id;
+
+        }
+
+    } else {
+        // No user is signed in.
+
+        document.getElementById("search_div").style.display = "none";
+        document.getElementById("user_div").style.display = "none";
+        document.getElementById("user_div").style.display = "none";
+        document.getElementById("login_div").style.display = "block";
+
+    }
+});
+
+function goSignUp() {
+    document.getElementById("sign_up_div").style.display = "block";
+    document.getElementById("login_div").style.display = "none";
+}
+
+function goLogin() {
+    document.getElementById("sign_up_div").style.display = "none";
+    document.getElementById("login_div").style.display = "block";
+}
+
+
+function signupFromFirebase() {
+    var fname = document.getElementById("sign_up_fname").value;
+    var lname = document.getElementById("sign_up_lname").value;
+    var email = document.getElementById("sign_up_email_field").value;
+    var password = document.getElementById("sign_up_password_field").value;
+
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+        .then((chart) => {
+            firebaseUserID = chart.user.uid;
+            firebase.firestore().collection('user').doc(chart.user.uid).set({
+                firstName: fname,
+                lastName: lname,
+                email: email,
+                password: password
+            });
+            console.log('successs');
+        }).catch(e => {
+        window.alert('Error happen' + e);
+    })
+}
+
+function loginFromFirebase() {
+    var userEmail = document.getElementById("login_email_field").value;
+    var userPass = document.getElementById("login_password_field").value;
+    if (/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(userEmail)) {
+        firebase.auth().signInWithEmailAndPassword(userEmail, userPass).catch(function (error) {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+
+            window.alert("Error : " + errorMessage);
+        });
+    } else {
+        window.alert("Your Email is not Valid");
+    }
+}
+
+function logoutFromFirebase() {
+    firebase.auth().signOut();
+}
+
+function seeBookmarks() {
+    alert('Will be added later !!');
+}
+
+/* login part on the upp */
+
+
+let jobsObject;
+let bookMarks;
+
+
 function getLocation() {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition((location) => {
@@ -24,12 +119,24 @@ _getJobs = (desc, location, fullTime, lat, long) => {
         });
 };
 
+function createButton(index, data) {
+    var button = document.createElement("button");
+    button.innerHTML = "button" + index;
+    button.value = data.id;
+    button.onclick = function () {
+        console.log(data.id);
+        console.log(firebaseUserID);
+        console.log('hereee');
+        firebase.firestore().collection('user').doc(firebaseUserID).collection('bookmarks').doc().set({
+            data
+        }, {merge: true});
+    };
+    document.body.appendChild(button);
+}
 
-let jobsObject = [];
 
-function jobs(param) {
-    console.log(jobsObject.length);
-    console.log('id : ', param);
+function seeBookmarks() {
+    firebase.firestore().collection('user').doc(firebaseUserID).collection('bookmarks').get().then((data) => renderJSON(data));
 }
 
 renderJSON = (response) => {
@@ -38,7 +145,7 @@ renderJSON = (response) => {
 
     jobsObject = [];
 
-    response.map((data) => {
+    response.map((data, index) => {
         jobsObject.push(data.id);
         console.log(JSON.stringify(jobsObject));
         // card
@@ -55,12 +162,17 @@ renderJSON = (response) => {
         data.company_logo ? companyLogo.src = data.company_logo : companyLogo.src = "https://upload.wikimedia.org/wikipedia/commons/9/93/No-logo.svg";
 
         //add bookmark
-        var addBookMark = document.createElement("button");
-        addBookMark.innerText = "Add to Bookmark";
-        addBookMark.addEventListener('click', function () {
-            alert('OnClick');
-            alert(data.id);
-        }, false);// fix hereee
+        /*
+        var button = document.createElement("button");
+        button.innerHTML = "button" + index;
+        button.value = "hello" + index;
+        container.appendChild(button);
+        button.onclick = function () {
+            console.log("event on button : ", this.value);
+        };
+        */
+
+        createButton(index, data);
 
         // position title
         var positionTitle = document.createElement("p");
@@ -84,7 +196,6 @@ renderJSON = (response) => {
 
         // create card with nodes
         container.appendChild(companyLogo);
-        container.appendChild(addBookMark);
         container.appendChild(positionTitle);
         container.appendChild(company);
         container.appendChild(location);
